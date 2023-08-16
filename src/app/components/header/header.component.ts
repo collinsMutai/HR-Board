2f900607c0fd9522717fd25d5ca5ea8dca80eab2
Collins Mutai <collinsfrontend@gmail.com>
2023-08-16 10:52:33 +0300

    navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
+
 import { JobsService } from '../../Service/jobs.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')! as HTMLDivElement
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
+ 
 export class HeaderComponent implements OnInit, OnDestroy {
   userIsAuthenticated = false;
   private authListenerSubs!: Subscription;
   returnUrl!: string;
-
+  update = false
   constructor(private router: Router, private JobsService: JobsService) {}
 
   ngOnInit(): void {
@@ -26,6 +33,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.userIsAuthenticated = isAuthenticated;
       }
     );
+  }
+  toggle() {
+    this.update = !this.update
   }
 
   onLogout(): void {
